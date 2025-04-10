7b36a5e80428a6d5a758bb95205408a1434c292c
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2025-04-10 21:56:28 +0530

    update the script file to exit when a step fails

--- integration-testing/scripts/run-integration.sh
+++ integration-testing/scripts/run-integration.sh
@@ -1,6 +1,8 @@
+set -e
 docker-compose up -d
 echo '🟡 - Waiting for database to be ready...'
 ./scripts/wait-for-it.sh "postgresql://postgres:mysecretpassword@localhost:5432/postgres" -- echo '🟢 - Database is ready!'
 npx prisma migrate dev --name init
-npm run test
+npm run test || TEST_EXIT_CODE=$?
 docker-compose down
+exit ${TEST_EXIT_CODE:-0}
